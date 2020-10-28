95e58b61dd6e6e0529053315a53595bc44467b85
ericmartineau <smartytime@gmail.com>
2020-10-27 17:23:31 -0700

    Updated package dependencies

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/ericm/sunny/sunny_flutter/.direnv/flutter/1.13.6"
+export "FLUTTER_ROOT=/Users/ericm/fvm/versions/1.22.1"
 export "FLUTTER_APPLICATION_PATH=/Users/ericm/sunny/plugin/mixpanel_analytics/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/ericm/sunny/sunny_flutter/.direnv/flutter/1.13.6/bin/cache/artifacts/engine/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/ericm/fvm/versions/1.22.1/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
